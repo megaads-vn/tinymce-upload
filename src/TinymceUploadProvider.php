91464fa1a93f490cff097cc1c71ca14f4f2399c5
Bach Nguyen <bachnx2303@gmail.com>
2021-03-24 15:17:47 +0700

    optimize: Change middleware alias name.

--- src/TinymceUploadProvider.php
+++ src/TinymceUploadProvider.php
@@ -3,7 +3,7 @@
 namespace Megaads\TinymceUpload;
 
 use Illuminate\Support\ServiceProvider;
-
+use Megaads\TinymceUpload\Middleware\UploadMiddleware;
 class TinymceUploadProvider extends ServiceProvider
 {
     /**
@@ -14,6 +14,8 @@ class TinymceUploadProvider extends ServiceProvider
     public function boot()
     {
         include __DIR__ . '/routes.php';
+        $router = App::make(Router::class);
+        $router->aliasMiddleware('mce_upload', UploadMiddleware::class);
     }
 
     /**
--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,6 @@ $locale = Request::segment(1);
 if ($locale == 'upload') {
     $locale = '';
 }
-Route::group(['middleware' => 'upload', 'prefix' => $locale], function () {
+Route::group(['middleware' => 'mce_upload', 'prefix' => $locale], function () {
     Route::post('/upload/tinymce', '\Megaads\TinymceUpload\Controllers\Services\UploadService@tinymceUpload');
 });
